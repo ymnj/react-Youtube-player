f6478af418b4b8978e293274e5751c84c2dc83c1
Tom <tomhung87@gmail.com>
2016-11-21 22:36:00 -0800

    Update webpack with webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
           presets: ['es2015', 'react']
